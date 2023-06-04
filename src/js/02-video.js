bf80d9ad1216717ada622a631e248f51ce0bbf93
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-06-04 17:29:45 +0300

    Task 02, Task 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+const STORAGE_KEY = 'videoplayer - current - time';
+
+player.on('timeupdate', throttle(onTimeUpdatePlayer, 1000));
+
+function onTimeUpdatePlayer(e) {
+  const timePlayer = e.seconds;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(timePlayer));
+}
+
+const storageTime = localStorage.getItem(STORAGE_KEY);
+console.log(storageTime);
+
+player.setCurrentTime(storageTime).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      // the time was less than 0 or greater than the video’s duration
+      break;
+
+    default:
+      // some other error occurred
+      break;
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const feedbackFormRef = document.querySelector('.feedback-form');
+const inputUserImailRef = document.querySelector('.feedback-form input');
+const inputUserMessageRef = document.querySelector('.feedback-form textarea');
+
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+feedbackFormRef.addEventListener('submit', onFeedbackFormSubmit);
+feedbackFormRef.addEventListener('input', throttle(onFeedbackFormInput, 500));
+
+populatForm();
+
+function onFeedbackFormInput() {
+  formData.email = inputUserImailRef.value;
+  formData.message = inputUserMessageRef.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populatForm() {
+  const savedInputForm = localStorage.getItem(STORAGE_KEY);
+
+  if (savedInputForm) {
+    const parseLocalStorage = JSON.parse(savedInputForm);
+    inputUserImailRef.value = parseLocalStorage.email;
+    inputUserMessageRef.value = parseLocalStorage.message;
+  }
+}
+
+function onFeedbackFormSubmit(e) {
+  e.preventDefault();
+
+  formData.email = inputUserImailRef.value;
+  formData.message = inputUserMessageRef.value;
+
+  console.log(formData);
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
